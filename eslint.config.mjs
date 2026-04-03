9e168e94f9162e388fc3acf6c15ef5b71f875279
kurateh <me@kurateh.com>
2026-04-03 15:16:50 +0900

    chore: save current changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,9 @@
+import kurateh from "@kurateh/eslint-plugin";
+import nextPlugin from "@next/eslint-plugin-next";
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...kurateh.configs.recommended,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -13,6 +12,67 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    files: ["*.config.*"],
+    rules: {
+      "import/no-unresolved": 0,
+    },
+  },
+  {
+    rules: {
+      "@kurateh/import-path": 1,
+
+      "import/order": [
+        1,
+        {
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+
+          "newlines-between": "always",
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+          ],
+
+          pathGroups: [
+            {
+              pattern: "~*/**",
+              group: "internal",
+            },
+            {
+              pattern: "@*/**",
+              group: "internal",
+            },
+          ],
+        },
+      ],
+
+      "prettier/prettier": [
+        1,
+        {},
+        {
+          usePrettierrc: true,
+        },
+      ],
+
+      "no-console": 1,
+
+      "no-restricted-imports": [2, {}],
+    },
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+    },
+  },
 ]);
 
 export default eslintConfig;
